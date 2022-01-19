882ca3d5b2b6f41257229ff96a17277294d41c92
Marco Mezzetti <ma.mezzetti@gmail.com>
2022-01-19 08:03:39 +0100

    Update

--- CSharp/SHA512RSA/Program.cs
+++ CSharp/SHA512RSA/Program.cs
@@ -2,16 +2,15 @@
 using System.Security.Cryptography;
 using System.Text;
 
-
-
-
-class RSACSPSample
+namespace SignWithRSA
 {
-    static void Main()
+    public class RSACSPSample
     {
-        try
+        static void Main()
         {
-            string privateKey = @"-----BEGIN RSA PRIVATE KEY-----
+            try
+            {
+                string privateKey = @"-----BEGIN RSA PRIVATE KEY-----
                                 MIIEowIBAAKCAQEA4YWrintlrUR6Xhoscdk0pFSI9U+Y7tDOFs491sC+m9wT6Jhl
                                 5SsR2QnPd/YbeCLzgmwG3dygL/Ld+1HycFz8Mab5p+OVI0QU8XHgGZwbbC69h1YZ
                                 WSnfkuunLYJUHOImH+8uydcjfDzK9owfLjW+dvX30KQ5IM44aVZuDxv8GJGATguM
@@ -39,110 +38,51 @@ class RSACSPSample
                                 ntnh+g/u4OpKoRUIxWZQRKfZM1kAlSo4GA+48qcn/0e2lHhs01ZY
                                 -----END RSA PRIVATE KEY-----";
 
+                string dataString = "ed610b4a03e4a360fa0e9ad0f3ba25105512acadad5cd5d4d2d8e2dc181662e33fc86a82f6349af7ee4cac5f3e43e8007ec1acf7ad13ece28dccc0c4b9157154";
 
-            // Create a UnicodeEncoder to convert between byte array and string.
-            ASCIIEncoding ByteConverter = new ASCIIEncoding();
-
-            string dataString = "ed610b4a03e4a360fa0e9ad0f3ba25105512acadad5cd5d4d2d8e2dc181662e33fc86a82f6349af7ee4cac5f3e43e8007ec1acf7ad13ece28dccc0c4b9157154";
-
-            // Create byte arrays to hold original, encrypted, and decrypted data.
-            byte[] originalData = ByteConverter.GetBytes(dataString);
-            byte[] signedData;
-            byte[] privateKeyData = ByteConverter.GetBytes(privateKey);
+                RSACSPSample P = new RSACSPSample();
+                string result = P.HashAndSignBytes(dataString, privateKey);
 
 
-
-
-
-
-
-
-            // Create a new instance of the RSACryptoServiceProvider class
-            // and automatically create a new key-pair.
-            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
-
-            //RSAalg.ImportParameters(privateKey);
-
-            // Export the key information to an RSAParameters object.
-            // You must pass true to export the private key for signing.
-            // However, you do not need to export the private key
-            // for verification.
-
-
-            RSAalg.ImportFromPem(privateKey.ToCharArray());
-
-            RSAParameters Key = RSAalg.ExportParameters(true);
-
-            //RSAalg.ImportRSAPublicKey(privateKeyData);
-
-
-
-
-            // Hash and sign the data.
-            signedData = HashAndSignBytes(originalData, Key);
-
-            //string result = System.Text.Encoding.UTF8.GetString(signedData);
-            string result = Convert.ToBase64String(signedData);
-
-
-            // Verify the data and display the result to the
-            // console.
-            if (VerifySignedHash(originalData, signedData, Key))
-            {
-                Console.WriteLine("The data was verified.");
                 Console.WriteLine(result);
             }
-            else
+            catch (ArgumentNullException)
             {
-                Console.WriteLine("The data does not match the signature.");
+                Console.WriteLine("The data was not signed or verified");
             }
         }
-        catch (ArgumentNullException)
+        public string HashAndSignBytes(string stringToSign, string pKey)
         {
-            Console.WriteLine("The data was not signed or verified");
+            try
+            {
+                // Create a new instance of RSACryptoServiceProvider using the
+                // key from RSAParameters.
+                ASCIIEncoding ByteConverter = new ASCIIEncoding();
+
+                byte[] originalData = ByteConverter.GetBytes(stringToSign);
+                byte[] dataToSign = ByteConverter.GetBytes(stringToSign);
+
+
+
+
+                RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+                RSAalg.ImportFromPem(pKey.ToCharArray());
+                RSAParameters Key = RSAalg.ExportParameters(true);
+
+                RSAalg.ImportParameters(Key);
+
+                // Hash and sign the data. Pass a new instance of SHA512
+                // to specify the hashing algorithm.
+                return Convert.ToBase64String(RSAalg.SignData(dataToSign, SHA512.Create()));
+            }
+            catch (CryptographicException e)
+            {
+
+                throw new ArgumentException(e.Message);
+            }
         }
+
+
     }
-    public static byte[] HashAndSignBytes(byte[] DataToSign, RSAParameters Key)
-    {
-        try
-        {
-            // Create a new instance of RSACryptoServiceProvider using the
-            // key from RSAParameters.
-            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
 
-            RSAalg.ImportParameters(Key);
-
-            // Hash and sign the data. Pass a new instance of SHA512
-            // to specify the hashing algorithm.
-            return RSAalg.SignData(DataToSign, SHA512.Create());
-        }
-        catch (CryptographicException e)
-        {
-            Console.WriteLine(e.Message);
-
-            return null;
-        }
-    }
-
-    public static bool VerifySignedHash(byte[] DataToVerify, byte[] SignedData, RSAParameters Key)
-    {
-        try
-        {
-            // Create a new instance of RSACryptoServiceProvider using the
-            // key from RSAParameters.
-            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
-
-            RSAalg.ImportParameters(Key);
-
-            // Verify the data using the signature.  Pass a new instance of SHA512
-            // to specify the hashing algorithm.
-            return RSAalg.VerifyData(DataToVerify, SHA512.Create(), SignedData);
-        }
-        catch (CryptographicException e)
-        {
-            Console.WriteLine(e.Message);
-
-            return false;
-        }
-    }
 }
