804d6e2972e37f45c3a7c5ef72ee9f2acb9430c5
Mez1092 <ma.mezzetti@gmail.com>
2022-02-23 13:20:43 +0100

    commit

--- Powershell/GAC.ps1
+++ Powershell/GAC.ps1
@@ -1,3 +1,9 @@
 #List DLL GAC
 
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\gacutil.exe" /l > "C:\TEMP\GAC.txt"
+
+
+#Note that you should be running PowerShell as an Administrator
+[System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")
+$publish = New-Object System.EnterpriseServices.Internal.Publish
+$publish.GacInstall("C:\Path\To\DLL.dll")
